49698eac188922f91b966c3c237e41e22933b7b7
Ben Loesch <benloesch@gmail.com>
2018-12-05 16:30:25 -0800

    Working on implementing tutorial level!

--- Assets/Scripts/UI Scripts/TogglePannel.cs
+++ Assets/Scripts/UI Scripts/TogglePannel.cs
@@ -1,11 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TogglePannel : MonoBehaviour {
 
-	public void Toggle(GameObject panel)
+    public GameObject menu;
+    public GameObject nextMenu;
+
+    public Text tutorialText;
+
+    public List<string> textList;
+    public int count;
+    public bool isPaused;
+
+    void Start()
     {
-        panel.SetActive(!panel.activeSelf);
+        count = 0;
+        tutorialText.text = textList[count];
+        On();
     }
+
+	public void On()
+    {
+        menu.SetActive(true);
+        isPaused = true;
+
+        //pause game
+        Time.timeScale = 0f;
+    }
+
+    public void Off()
+    {
+        menu.SetActive(false);
+        isPaused = false;
+        count++;
+        
+        MoveText(textList[count]);
+
+        //unpause game
+        Time.timeScale = 1f;
+    }
+
+    public void MoveText(string text)
+    {
+        string[] textData = text.Split(':');
+
+        tutorialText.text = textData[0];
+        int x = 0;
+        Int32.TryParse(textData[1], out x);
+        int y = 0;
+        Int32.TryParse(textData[2], out y);
+
+        //Move menu to new coords
+        menu.RectTransform.localPosition += new Vector3(x, 0f, y);
+    }
+
 }
